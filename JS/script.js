31cb9c0d22daa9eb1f5ac9bfd27c5d6340e7a9c9
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-29 19:46:41 +0100

    add  asynchronous email loading with loading indicator

--- JS/script.js
+++ JS/script.js
@@ -2,20 +2,28 @@ console.log('VUE-OK', Vue);
 
 const { createApp } = Vue;
 
-const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 const app = createApp({
     data() {
         return {
-            emails: []
+            emails: [],
+            loading: true
         };
     },
     created() {
+        if (!this.loading) {
+            return;
+        }
         for (let i = 0; i < 10; i++) {
             axios.get(endpoint).then(res => {
                 this.emails.push(res.data.response);
+                if (this.emails.length === 10) {
+                    this.loading = false;
+                }
             });
         }
     },
 });
+
 app.mount('#root');
